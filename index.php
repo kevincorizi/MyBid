f9c0b5ede0a765501cae54ee65c40e1ced7ea38e
Kevin Corizi <kevincorizi@outlook.com>
2017-06-07 18:00:23 +0200

    First draft of style

--- index.php
+++ index.php
@@ -1,7 +1,44 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: kevin
- * Date: 07/06/2017
- * Time: 16:16
- */
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="./css/style.css"/>
+</head>
+<body>
+    <header>
+        <div id="logo">
+            <img src="x">
+            MyBid
+        </div>
+        <div id="slogan">
+            <p>the best bidding platform ever</p>
+        </div>
+    </header>
+    <main>
+    <aside>
+        <ul id="app_actions">
+            <?php
+                if(true /* IS_LOGGED */)
+                    echo '<li>Your profile</li>'
+            ?>
+            <li>Place an offer</li>
+        </ul>
+        <ul id="user_actions">
+            <?php
+            if(true /* IS_LOGGED */)
+                echo '<li>Login</li>';
+            else
+                echo '<li>Logout</li>';
+            ?>
+        </ul>
+    </aside>
+    <section id="best_offer">
+        <h1 id="best_offer_title">Current best offer</h1>
+        <article>
+            <p id="best_offer_value">9.99$</p>
+            <p id="best_offer_bidder">from <span class="bold_text">il_kappa</span></p>
+        </article>
+        <p id="best_offer_message">Care to beat him? <span class="bold_text">Place an offer!</span></p>
+    </section>
+    </main>
+</body>
+</html>
